ba93d62e83e63fc3e980fa34aa19646b9a482ab3
Adrian <adrianhenry2115@gmail.com>
2022-05-08 14:52:58 -0400

    mutations, quesries and server info filled, data for searchbooks and savebooks filled, updated signup and login functionalities

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,42 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type User {
-    _id: ID
-    username: String
-    email: String
-    savedBooks: [Books]
-},
-type Book {
-    _id: ID
-    authors: String
-    description: String!
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
     image: String
     link: String
-    title: String!
-}
-
-type Query {
+  }
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  type Query {
     me: User
-    users: [User]
-    user(username: String!): User
-    
-}
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+  }
+`;
 
-`
+module.exports = typeDefs;
